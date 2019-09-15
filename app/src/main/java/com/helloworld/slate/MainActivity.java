8b3dacadcd19878e97c8862984f99ce25dcfba3c
Vishisht <jain322@purdue.edu>
2019-09-15 01:43:34 -0400

    Updated PaintNew to PainViewNew

--- app/src/main/java/com/helloworld/slate/MainActivity.java
+++ app/src/main/java/com/helloworld/slate/MainActivity.java
@@ -1,16 +1,23 @@
 package com.helloworld.slate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        PaintView paintView = new PaintView(this);
-        setContentView(paintView);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_paint_view_new);
+        Intent intent = new Intent(MainActivity.this, PaintViewNew.class);
+        startActivity(intent);
+
+
 
     }
+
 }
